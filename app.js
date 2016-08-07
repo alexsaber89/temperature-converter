19c2ccc45cfee2f515295525f7c79de36ff64d54
Alex Saber <Alex Saber>
2016-08-07 00:34:39 -0500

    extracted inputValidation() from determineConversion(); cleaned up js comments

--- app.js
+++ app.js
@@ -1,75 +1,94 @@
 var userInput = document.getElementById("input_field");
 var convertedTempDiv = document.getElementById("converted_temp");
 
-//temperature conversion functions
-function toCelsius () {
-  var convertedTemp = (userInput.value - 32) * .5556;
-  var result;
-  console.log("Converted temp: ",convertedTemp);
-  if (convertedTemp > 32) {
-    result = "<p class='celsius red'>" + convertedTemp + "</p>";
-  } else if (convertedTemp < 0) {
-    result = "<p class='celsius blue'>" + convertedTemp + "</p>";
-  } else {
-    result = "<p class='celsius'>" + convertedTemp + "</p>";
-  }
-  convertedTempDiv.innerHTML = result;
-}
-
-function toFahrenheit () {
-  var convertedTemp = (userInput.value * 1.8) + 32;
-  var result;
-  console.log("Converted temp: ",convertedTemp);
-  if (convertedTemp > 90) {
-    result = "<p class='celsius red'>" + convertedTemp + "</p>";
-  } else if (convertedTemp < 32) {
-    result = "<p class='celsius blue'>" + convertedTemp + "</p>";
-  } else {
-    result = "<p class='celsius'>" + convertedTemp + "</p>";
-  }
-  convertedTempDiv.innerHTML = result;
-}
-
 //Get a reference to the button element in the DOM
 var button = document.getElementById("converter");
 var clearButton = document.getElementById("clear");
 
-//Figure out if the user pressed Enter after entering a value
-
-
-//Figure out which radio button the user clicked, then call the relevant function
-button.addEventListener("click", function determineConverter() {
+//check if user checked a radio button
+//check if user entered a value
+function inputValidation() {
+  var userInputText = document.getElementById("input_field").value;
   var radioButtons = document.getElementsByName("temp_input");
-  for (var i = 1; i < radioButtons.length; i++) {
-    if(radioButtons[i].checked && radioButtons[i].value === "celsius") {
-        console.log(radioButtons[i].value + " has been checked");
-        console.log("toCelsius() will run");
-        toCelsius();
-        break;
-    } else if (radioButtons[i].checked && radioButtons[i].value === "fahrenheit") {
-        console.log(radioButtons[i].value + " has been checked");
-        console.log("toFahrenheit() will run");
-        toFahrenheit();
-      break;
-    } else if (userInput.value === "") {
-        alert("Please enter a temperature");
-        break;
-    } else if (!radioButtons[i].checked && !radioButtons[i+1].checked) {
-        console.log("No scale selected");
-        alert("Please select desired temperature scale.");
-        break;
-    }
+  console.log("Radio buttons value: ",radioButtons);
+
+  //passes both the requested scale and the user's input to determineConverter()
+  if (radioButtons[0].checked) {
+    determineConverter(radioButtons[0].value, userInputText);
+  } else if (radioButtons[1].checked) {
+    determineConverter(radioButtons[1].value, userInputText);
+  } else {
+    console.log("No scale selected");
+    alert("Please select desired temperature scale.");
+  }
+  if (userInputText === "") {
+    alert("Please enter a temperature");
+  }
+}
+
+//determine which scale conversion to run
+//uses scale passed in by inputValidation()
+function determineConverter(scale, userInputText) {
+  console.log("Scale: ",scale);
+  if(scale === "celsius") {
+    console.log(scale + " has been checked");
+    console.log("toCelsius() will run");
+
+    //passes user's input as argument for toCelsius()
+    toCelsius(userInputText);
+  } else {
+    console.log(scale + " has been checked");
+    console.log("toFahrenheit() will run");
+
+    //passes user's input as argument for toFahrenheit()
+    toFahrenheit(userInputText);
+  }
+}
+
+//celsius temperature conversion functions
+function toCelsius(rawTemp) {
+  var convertedToCelsius = (rawTemp - 32) * .5556;
+  var result;
+  console.log("Converted temp: ",convertedToCelsius);
+  if (convertedToCelsius > 32) {
+    result = "<p class='celsius red'>" + convertedToCelsius + "</p>";
+  } else if (convertedToCelsius < 0) {
+    result = "<p class='celsius blue'>" + convertedToCelsius + "</p>";
+  } else {
+    result = "<p class='celsius'>" + convertedToCelsius + "</p>";
+  }
+  convertedTempDiv.innerHTML = result;
+}
+
+//fahrenheit temperature conversion functions
+function toFahrenheit(rawTemp) {
+  var convertedToFahrenheit = (rawTemp * 1.8) + 32;
+  var result;
+  console.log("Converted temp: ",convertedToFahrenheit);
+  if (convertedToFahrenheit > 90) {
+    result = "<p class='celsius red'>" + convertedToFahrenheit + "</p>";
+  } else if (convertedToFahrenheit < 32) {
+    result = "<p class='celsius blue'>" + convertedToFahrenheit + "</p>";
+  } else {
+    result = "<p class='celsius'>" + convertedToFahrenheit + "</p>";
+  }
+  convertedTempDiv.innerHTML = result;
+}
+
+//Figure out if the user pressed Enter after entering a value
+userInput.addEventListener("keyup", function(event) {
+  event.preventDefault();
+  if (event.keyCode == 13) {
+    determineConverter();
   }
 });
 
+//Determine which radio button the user clicked, then run callback inputValidation
+button.addEventListener("click", inputValidation);
+
 //Use clear button to clear user's input
-clearButton.addEventListener("click", function clearInputField() {
-  userInput.value = "";
+function clearInputField() {
+  userInput = "";
   console.log("User input cleared");
-});
-
-//   // console.log("event", clickEvent);
-// }
-
-// Assign a function to be executed when the button is clicked
-// button.addEventListener("click", determineConverter);
+};
+clearButton.addEventListener("click", clearInputField);
